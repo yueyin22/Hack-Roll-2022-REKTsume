85e1951d7bce944962ea02008681c5d06e7242b4
Fuxi <fuxi_xia@u.nus.edu>
2022-01-07 14:29:25 +0800

    Add script for website

---
+++ main.js
@@ -0,0 +1,31 @@
+const studentName = document.querySelector("#name");
+const education = document.querySelector("#education");
+const experience = document.querySelector("#experience");
+const programmingLanguage = document.querySelector("#programmingLanguage");
+const techSkill = document.querySelector("#techSkill");
+const softSkill = document.querySelector("#softSkill");
+const project = document.querySelector("#project");
+const volunteering = document.querySelector("#volunteering");
+const button = document.querySelector("#create");
+
+function extractText(element) {
+  return element.value;
+}
+
+function serialise() {
+  var info = {
+    studentName: extractText(studentName),
+    education: extractText(education),
+    experience: extractText(experience),
+    programmingLanguage: extractText(programmingLanguage),
+    techSkill: extractText(techSkill),
+    softSkill: extractText(softSkill),
+    project: extractText(project),
+    volunteering: extractText(volunteering),
+  };
+  return Object.fromEntries(Object.entries(info).filter(([_, v]) => v != null));
+}
+
+button.addEventListener("click", () => {
+  return serialise();
+});
