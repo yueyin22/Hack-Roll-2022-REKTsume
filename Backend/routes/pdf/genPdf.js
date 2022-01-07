d1ed40e843a03b5271c58a9364c22adbf7874037
Fuxi <fuxi_xia@u.nus.edu>
2022-01-07 12:15:46 +0800

    Add resume db and code to load db

--- Backend/routes/pdf/genPdf.js
+++ Backend/routes/pdf/genPdf.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 const { PythonShell } = require("python-shell");
 
+// Change to post
 router.get("/pdf", (req, res) => {
   let options = {
     mode: "text",
     pythonPath: "C:/Python38/python.exe",
     pythonOptions: ["-u"], // get print results in real-time
-    args: "",
+    args: JSON.stringify(req.body),
   };
   PythonShell.run("./routes/pdf/genPdf.py", options, function (err, results) {
     if (err) throw err;
