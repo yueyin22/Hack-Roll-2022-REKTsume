e6041fc22594a7dbb1b19e5b83714c1e700e9912
yueyin22 <e0556336@u.nus.edu>
2022-01-07 16:36:26 +0800

    js: added function for form validation

--- main.js
+++ main.js
@@ -29,3 +29,20 @@ function serialise() {
 button.addEventListener("click", () => {
   return serialise();
 });
+
+(function () {
+  'use strict'
+  const forms = document.querySelectorAll('.requires-validation')
+  Array.from(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
+  
