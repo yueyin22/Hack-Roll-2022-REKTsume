ee4351b613b20ffe83692e5219c6ce3cca8c319d
Fuxi <fuxi_xia@u.nus.edu>
2022-01-08 11:09:32 +0800

    Modify port

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 app.options("/pdf", cors());
 app.options("/get-pdf", cors());
 
